902238542fac342904da8d0ee665f73f2a12cf28
Justin Beckwith <justin.beckwith@gmail.com>
2025-01-11 18:33:43 -0800

    Update dependency discord-interactions to v4

--- src/index.ts
+++ src/index.ts
@@ -219,7 +219,7 @@ async function verifyDiscordRequest(
 	const isValidRequest =
 		signature &&
 		timestamp &&
-		verifyKey(body, signature, timestamp, env.DISCORD_PUBLIC_KEY);
+		await verifyKey(body, signature, timestamp, env.DISCORD_PUBLIC_KEY);
 	if (!isValidRequest) {
 		return {isValid: false};
 	}
